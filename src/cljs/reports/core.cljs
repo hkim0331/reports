1a35070f70c05ec0e115b4f371d7eddb5a8ac5fa
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2022-06-02 16:47:05 +0900

    updating

--- src/cljs/reports/core.cljs
+++ src/cljs/reports/core.cljs
@@ -195,8 +195,8 @@
    (for [[i u] ((filters @random?) (map-indexed vector @users))]
      [:div.columns {:key i}
       [:div.column.is-one-quarter
-       [:a {:href (report-url u)
-            :class (if (= u "hkimura") "hkimura" "other")}
+       [:a {:href (report-url u)}
+            :class (if (= u "hkimura") "hkimura" "other")
            u]
        " "
        (get @titles u)]
